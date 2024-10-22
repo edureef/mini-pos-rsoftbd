dc0685db18f216e1ae1594409341dbe972761c05
ah-rifat <rifat266@gmail.com>
2024-10-22 23:20:36 +0600

    purchase data migration done

---
+++ app/Models/PurchaseProduct.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PurchaseProduct extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'supplier_id',
+        'products',
+        'payment_status',
+        'netTotal',
+        'paidAmount',
+        'discount',
+        'dueAmount',
+        'grandTotal',
+    ];
+}
